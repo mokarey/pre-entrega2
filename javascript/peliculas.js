6dde1b7ed4b4b4bb8fc00915594a9923869a518e
Valentin <107222626+mokarey@users.noreply.github.com>
2022-09-11 21:16:59 -0300

    fetch 2

--- javascript/peliculas.js
+++ javascript/peliculas.js
@@ -1,16 +1,13 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    fetchData()
-})
+const URL = "javascript/peliculas.json"
 
-const fetchData = async ()=> {
-    try{
-        const res = await fetch ('peliculas.json')
-        const data = await res.json()
-        console.log(data)
-    }catch(error){
-            console.log(error)
-    }
-}
+
+const cargarPeliculas = async ()=>{
+    await fetch('javascript/peliculas.json')
+         .then((response)=> response.json())
+         .then((data)=>console.table(data))
+           
+ }
+
 
 const card = document.getElementById('peliCard')
 const peliculaGratis = []
@@ -55,101 +52,101 @@ function agregarPelisGratis() {
 
 agregarPelisGratis()
 
-function crearCards(){
-    peliculaGratis.forEach
-        (peliculaFree => {
-        card.innerHTML += `
-            <div class="card col" style="width: 18rem;">
-                <img src="${peliculaFree.foto}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h4>${peliculaFree.titulo}</h4>
-                <p class="text-muted">${peliculaFree.generos}</p>
-                <p><b>${peliculaFree.clasificacion}</b></p>
-                <a href="" id="btnCarritoPi" class="btn btn-primary">Ver <b>gratis</b> ahora!</a>
-            </div>
-            `;
-    }
-)} 
+//function crearCards(){
+//    peliculaGratis.forEach
+//        (peliculaFree => {
+//        card.innerHTML += `
+//            <div class="card col" style="width: 18rem;">
+//                <img src="${peliculaFree.foto}" class="card-img-top" alt="...">
+//            <div class="card-body">
+//                <h4>${peliculaFree.titulo}</h4>
+//                <p class="text-muted">${peliculaFree.generos}</p>
+//                <p><b>${peliculaFree.clasificacion}</b></p>
+//                <a href="" id="btnCarritoPi" class="btn btn-primary">Ver <b>gratis</b> ahora!</a>
+//           </div>
+//            `;
+//    }
+//)} 
 
-crearCards()
+//crearCards()
 
 
-let stockPeliculas = [{ 
-    id: 1, 
-    titulo: "The revenant", 
-    generos: "Supervivencia, Aventura, Drama", 
-    clasificacion: "+16", precio: 1499, 
-    Img: ''
-},
-{ 
-    id: 2, 
-    titulo: "Once upon a time in Hollywood", 
-    generos: "Drama, Comedia dramatica, Comedia cinematografica", 
-    clasificacion: "+16", 
-    precio: 2999, 
-    Img: ''},
-{ 
-    id: 3, 
-    titulo: "Bohemian Rhapsody", 
-    generos: "Musical, Musica, Drama", 
-    clasificacion: "+16", 
-    precio: 1999, 
-    Img: ''},
-{ 
-    id: 4, 
-    titulo: "Parasite", 
-    generos: "Thriller, Drama, Comedia cinematografica", 
-    clasificacion: "ATP", 
-    precio: 2999, 
-    Img: ''},
-{ 
-    id: 5, 
-    titulo: "Whiplash", 
-    generos: "Musica, Drama, Cine independiente", 
-    clasificacion: "ATP", 
-    precio: 1499, 
-    Img: ''},
-{ 
-    id: 6, 
-    titulo: "1917", 
-    generos: "Accion, Guerra, Ficcion", 
-    clasificacion: "+16",  
-    precio: 1999, 
-    Img: '',
-}]
+//let stockPeliculas = [{ 
+//   id: 1, 
+//    titulo: "The revenant", 
+//  generos: "Supervivencia, Aventura, Drama", 
+//clasificacion: "+16", precio: 1499, 
+//Img: ''
+//},
+//{ 
+//    id: 2, 
+//   titulo: "Once upon a time in Hollywood", 
+// generos: "Drama, Comedia dramatica, Comedia cinematografica", 
+//clasificacion: "+16", 
+// precio: 2999, 
+// Img: ''},
+//{ 
+//   id: 3, 
+//  titulo: "Bohemian Rhapsody", 
+// generos: "Musical, Musica, Drama", 
+// clasificacion: "+16", 
+//  precio: 1999, 
+// Img: ''},
+//{ 
+//   id: 4, 
+// titulo: "Parasite", 
+// generos: "Thriller, Drama, Comedia cinematografica", 
+//  clasificacion: "ATP", 
+// precio: 2999, 
+//  Img: ''},
+//{ 
+//   id: 5, 
+//  titulo: "Whiplash", 
+// generos: "Musica, Drama, Cine independiente", 
+//clasificacion: "ATP", 
+//precio: 1499, 
+// Img: ''},
+//{ 
+//  id: 6, 
+//titulo: "1917", 
+// generos: "Accion, Guerra, Ficcion", 
+//clasificacion: "+16",  
+// precio: 1999, 
+// Img: '',
+//}]
 
-const peliPaga = document.getElementById('peliCardPaga')
+//const peliPaga = document.getElementById('peliCardPaga')
 const carritoDiv = document.getElementById('carrito')
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
-function crearCard() {
-    stockPeliculas.forEach((pelicula) => {
-        peliPaga.innerHTML += `
-        <div class="card col" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h4>${pelicula.titulo}</h4>
-            <p class="text-muted">${pelicula.generos}</p>
-            <p>${pelicula.clasificacion}</p>
-            <button id="btnCarrito${pelicula.id}" class=""> Agregar al carrito <b>$${pelicula.precio}</b>.</button>
-        </div>`
-    })
-    agregarFuncionalidad()
-}
+//function crearCard() {
+//  stockPeliculas.forEach((pelicula) => {
+//    peliPaga.innerHTML += `
+//   <div class="card col" style="width: 18rem;">
+//     <img src="" class="card-img-top" alt="...">
+//<div class="card-body">
+// <h4>${pelicula.titulo}</h4>
+//<p class="text-muted">${pelicula.generos}</p>
+//<p>${pelicula.clasificacion}</p>
+//<button id="btnCarrito${pelicula.id}" class=""> Agregar al carrito <b>$${pelicula.precio}</b>.</button>
+//</div>`
+// })
+//  agregarFuncionalidad()
+//}
 
 
 
 
 
-function agregarFuncionalidad() {
-    stockPeliculas.forEach((pelicula) =>{
-        document.getElementById (`btnCarrito${pelicula.id}`).addEventListener("click", ()=> {
-        console.log(pelicula)
-        agregarAlCarrito(pelicula)
-        })
-    })
-}
+//function agregarFuncionalidad() {
+//   stockPeliculas.forEach((pelicula) =>{
+//document.getElementById (`btnCarrito${pelicula.id}`).addEventListener("click", ()=> {
+// console.log(pelicula)
+//  agregarAlCarrito(pelicula)
+//    })
+//  })
+//}
 
 
 function agregarAlCarrito(pelicula){
@@ -193,7 +190,7 @@ function eliminarPelicula(){
     })
 }
 
-crearCard()
+//crearCard()
 cargarCarrito()
 
 Swal.fire({
@@ -202,7 +199,5 @@ Swal.fire({
     text:'Este sitio contiene contenido explicito.',
 }) 
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    fetchData()
-})
+
 
